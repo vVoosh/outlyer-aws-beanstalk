6faf7e250b4ba0d7e6e751bddcd388b180fe83c9
Felipe Lopes <felipe@vvoosh.com>
2018-06-21 17:18:08 +0100

    New file also

--- .ebextensions/outlyer/hooks/99start_outlyer.sh
+++ .ebextensions/outlyer/hooks/99start_outlyer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # .ebextensions/outlyer/hooks/99start_outlyer.sh
-if [ -e /etc/init.d/outlyer-agent ]; then
+if [ -e /etc/init/outlyer-agent.conf ]; then
   initctl start outlyer-agent
 fi
--- .ebextensions/outlyer/hooks/99stop_outlyer.sh
+++ .ebextensions/outlyer/hooks/99stop_outlyer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # .ebextensions/outlyer/hooks/99stop_outlyer.sh
-if [ -e /etc/init.d/outlyer-agent ]; then
+if [ -e /etc/init/outlyer-agent.conf ]; then
   initctl stop outlyer-agent || true
 fi
